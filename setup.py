6fdf0f03cb35322a343d1c9170b9d682dc5f0048
yy <mengyy_linux@163.com>
2019-01-14 21:58:54 +0800

    Remove Pillow dependency

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data = True,
     package_data = {'': 'booking/station_list.json'},
+    # install_requires=["Pillow>=5.4.1", "hack12306>=0.1.11", "click==7.0", "six>=1.12.0"],
     install_requires=["Pillow>=5.4.1", "hack12306>=0.1.11", "click==7.0", "six>=1.12.0"],
     entry_points={
         'console_scripts': [
